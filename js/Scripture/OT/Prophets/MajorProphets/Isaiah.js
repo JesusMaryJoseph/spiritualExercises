0117ba65311eb66f098c13713cca9a8778e47749
Bob Englert <mre29073@yahoo.com>
2021-09-09 14:07:00 -0600

    Add files via upload

---
+++ js/Scripture/OT/Prophets/MajorProphets/Isaiah.js
@@ -0,0 +1,104 @@
+
+    // Children of sNode0400 (Isaiah:)
+	
+    var sNode04002 = {
+        id: "s04002",
+        name: "About:",
+        data:{"width": "60","height": "23"},
+        children:[]			
+    };
+
+	    // Children of sNode04000 (Isaiah<br>1-39:)
+		
+	    var sNode040000 = {
+	        id: "s040000",
+	        name: "Indictment Of<br>Israel & Judah:",
+	        data:{"width": "120","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040001 = {
+	        id: "s040001",
+	        name: "The Book Of<br>Emmanuel:",
+	        data:{"width": "100","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040002 = {
+	        id: "s040002",
+	        name: "Oracles Against The<br>Foreign Nations:",
+	        data:{"width": "155","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040003 = {
+	        id: "s040003",
+	        name: "Apocalypse<br>Of Isaiah:",
+	        data:{"width": "95","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040004 = {
+	        id: "s040004",
+	        name: "The Lord Alone<br>Israel's & Judah's<br>Salvation:",
+	        data:{"width": "135","height": "57"},
+	        children:[]			
+	    };
+		
+	    var sNode040005 = {
+	        id: "s040005",
+	        name: "The Lord<br>Zion's Avenger:",
+	        data:{"width": "125","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040006 = {
+	        id: "s040006",
+	        name: "Historical<br>Appendix:",
+	        data:{"width": "90","height": "39"},
+	        children:[]			
+	    };;
+	
+    var sNode04000 = {
+        id: "s04000",
+        name: "Isaiah<br>1-39:",
+        data:{"width": "65","height": "39"},
+        children:[sNode040000,sNode040001,sNode040002,sNode040003,sNode040004,sNode040005,sNode040006,sNode04002]			
+    };
+
+	    // Children of sNode04001 (Isaiah<br>40-66:)
+		
+	    var sNode040010 = {
+	        id: "s040010",
+	        name: "The Lord's Glory In<br>Israel's Liberation:",
+	        data:{"width": "145","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040011 = {
+	        id: "s040011",
+	        name: "Expiation Of Sin<br>Spiritual Liberation:",
+	        data:{"width": "155","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040012 = {
+	        id: "s040012",
+	        name: "Isaiah<br>56-66:",
+	        data:{"width": "55","height": "39"},
+	        children:[]			
+	    };
+	
+    var sNode04001 = {
+        id: "s04001",
+        name: "Isaiah<br>40-66:",
+        data:{"width": "60","height": "39"},
+        children:[sNode040010,sNode040011,sNode040012,sNode04002]			
+    };
+
+var sNode0400 = {
+    id: "s0400",
+    name: "Isaiah:",
+    data:{"width": "70","height": "23"},
+    children:[sNode04000,sNode04001,sNode04002]			
+};
---
+++ js/Scripture/OT/Prophets/MajorProphets/Jeremiah.js
@@ -0,0 +1,144 @@
+
+
+    // Children of sNode0401 (Jeremiah:)
+
+	    // Children of sNode04010 (Jeremiah<br>1-29:)
+		
+	    var sNode040100 = {
+	        id: "s040100",
+	        name: "Oracles In The<br>Days Of Josiah:",
+	        data:{"width": "130","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040101 = {
+	        id: "s040101",
+	        name: "Oracles Primarily<br>From The Days<br>Of Jehoiakim:",
+	        data:{"width": "145","height": "57"},
+	        children:[]			
+	    };
+		
+	    var sNode040102 = {
+	        id: "s040102",
+	        name: "Oracles In<br>The Last Years<br>Of Jerusalem:",
+	        data:{"width": "120","height": "57"},
+	        children:[]			
+	    };
+		
+	    var sNode040103 = {
+	        id: "s040103",
+	        name: "The Temple<br>Sermon:",
+	        data:{"width": "95","height": "39"},
+	        children:[]			
+	    };
+		
+	    var sNode040104 = {
+	        id: "s040104",
+	        name: "Controversies With<br>The False Prophets:",
+	        data:{"width": "150","height": "39"},
+	        children:[]			
+	    };
+	
+    var sNode04010 = {
+        id: "s04010",
+        name: "Jeremiah<br>1-29:",
+        data:{"width": "75","height": "39"},
+        children:[sNode040100,sNode040101,sNode040102,sNode040103,sNode040104]			
+    };
+
+	    // Children of sNode04011 (Jeremiah 30-52<br>& Lamentations:)
+
+		    // Children of sNode040110 (Jeremiah 30-52:)
+			
+		    var sNode0401100 = {
+		        id: "s0401100",
+		        name: "Oracles Of The<br>Restoration Of<br>Israel & Judah:",
+		        data:{"width": "120","height": "57"},
+		        children:[]			
+		    };
+			
+		    var sNode0401101 = {
+		        id: "s0401101",
+		        name: "Jeremiah And The<br>Fall Of Jerusalem:",
+		        data:{"width": "140","height": "39"},
+		        children:[]			
+		    };
+			
+		    var sNode0401102 = {
+		        id: "s0401102",
+		        name: "Oracles Against<br>The Nations:",
+		        data:{"width": "125","height": "39"},
+		        children:[]			
+		    };
+			
+		    var sNode0401103 = {
+		        id: "s0401103",
+		        name: "Historical<br>Appendix:",
+		        data:{"width": "85","height": "39"},
+		        children:[]			
+		    };
+		
+	    var sNode040110 = {
+	        id: "s040110",
+	        name: "Jeremiah 30-52:",
+	        data:{"width": "125","height": "23"},
+	        children:[sNode0401100,sNode0401101,sNode0401102,sNode0401103]			
+	    };
+
+		    // Children of sNode040111 (Lamentations:)
+			
+		    var sNode0401110 = {
+		        id: "s0401110",
+		        name: "Oracles Of The<br>Restoration Of<br>Israel & Judah:",
+		        data:{"width": "120","height": "57"},
+		        children:[]			
+		    };
+			
+		    var sNode0401111 = {
+		        id: "s0401111",
+		        name: "Jeremiah And The<br>Fall Of Jerusalem:",
+		        data:{"width": "140","height": "39"},
+		        children:[]			
+		    };
+			
+		    var sNode0401112 = {
+		        id: "s0401112",
+		        name: "Oracles Against<br>The Nations:",
+		        data:{"width": "125","height": "39"},
+		        children:[]			
+		    };
+			
+		    var sNode0401113 = {
+		        id: "s0401113",
+		        name: "Historical<br>Appendix:",
+		        data:{"width": "85","height": "39"},
+		        children:[]			
+		    };
+			
+		    var sNode0401114 = {
+		        id: "s0401114",
+		        name: "Historical<br>Appendix:",
+		        data:{"width": "85","height": "39"},
+		        children:[]			
+		    };
+		
+	    var sNode040111 = {
+	        id: "s040111",
+	        name: "Lamentations:",
+	        data:{"width": "110","height": "23"},
+	        children:[]			
+	    };
+	
+    var sNode04011 = {
+        id: "s04011",
+        name: "Jeremiah 30-52<br>& Lamentations:",
+        data:{"width": "140","height": "39"},
+        children:[sNode040110,sNode040111]			
+    };
+
+var sNode0401 = {
+    id: "s0401",
+    name: "Jeremiah:",
+    data:{"width": "95","height": "23"},
+    children:[sNode04010,sNode04011]			
+};
