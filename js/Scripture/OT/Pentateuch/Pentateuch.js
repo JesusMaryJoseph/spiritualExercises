0a52aac4342c5b88a1a8cae88c10b3189756cc11
Bob Englert <mre29073@yahoo.com>
2021-09-09 14:17:17 -0600

    Add files via upload

---
+++ js/Scripture/OT/Pentateuch/Pentateuch.js
@@ -0,0 +1,40 @@
+
+
+	        // Children of sNode00 (The Pentateuch Plus)
+
+	            // Children of sNode000 (The Pentateuch)
+				
+                var sNode0000 = {
+	                id: "s0000",
+		            name: "Genesis:",
+		            data:{"width": "70","height": "23"},
+		            children:[]			
+	            };
+				
+                var sNode0001 = {
+	                id: "s0001",
+		            name: "Exodus:",
+		            data:{"width": "65","height": "23"},
+		            children:[]			
+	            };
+				
+                var sNode0002 = {
+	                id: "s0002",
+		            name: "Leviticus:",
+		            data:{"width": "75","height": "23"},
+		            children:[]			
+	            };
+				
+                var sNode0003 = {
+	                id: "s0003",
+		            name: "Numbers:",
+		            data:{"width": "75","height": "23"},
+		            children:[]			
+	            };
+				
+                var sNode0004 = {
+	                id: "s0004",
+		            name: "Deuteronomy:",
+		            data:{"width": "105","height": "23"},
+		            children:[]			
+	            };
