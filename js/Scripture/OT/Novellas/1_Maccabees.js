615a5b96293d410d87008570d32adc6523361cb4
Bob Englert <mre29073@yahoo.com>
2021-09-09 14:14:40 -0600

    Add files via upload

---
+++ js/Scripture/OT/Novellas/1_Maccabees.js
@@ -0,0 +1,140 @@
+
+
+    // Children of sNode0230 (1st<br>Maccabees:)
+
+	    // Children of sNode02300 (<u>1st Maccabees</u><br>Part One:)
+
+		    // Children of sNode023000 (Crisis And<br>Response:)
+
+		    	// Children of sNode0230000 (Chapter<br>1:)
+
+			    var sNode02300000 =  {
+			        id: "s02300000",
+				    name: "From Alexander<br>To Antiochus:",
+				    data:{"width": "115","height": "39"},
+				    children:[]			
+			    };
+
+			    var sNode02300001 =  {
+			        id: "s02300001",
+				    name: "Lawless<br>Jews:",
+				    data:{"width": "65","height": "39"},
+				    children:[]			
+			    };
+
+			    var sNode02300002 =  {
+			        id: "s02300002",
+				    name: "Antiochus<br>In Egypt:",
+				    data:{"width": "85","height": "39"},
+				    children:[]			
+			    };
+
+			    var sNode02300003 =  {
+			        id: "s02300003",
+				    name: "Robbery Of<br>The Temple:",
+				    data:{"width": "95","height": "39"},
+				    children:[]			
+			    };
+
+			    var sNode02300004 =  {
+			        id: "s02300004",
+				    name: "Attack And<br>Occuption:",
+				    data:{"width": "95","height": "39"},
+				    children:[]			
+			    };
+
+			    var sNode02300005 =  {
+			        id: "s02300005",
+				    name: "Religious<br>Persecution:",
+				    data:{"width": "110","height": "39"},
+				    children:[]			
+			    };
+
+		    var sNode0230000 =  {
+		        id: "s0230000",
+			    name: "Chapter<br>1:",
+			    data:{"width": "95","height": "39"},
+			    children:[sNode02300000,sNode02300001,sNode02300002,sNode02300003,sNode02300004,sNode02300005]			
+		    };
+
+		    	// Children of sNode0230001 (Chapter<br>2:)
+
+			    var sNode02300010 =  {
+			        id: "s02300010",
+				    name: "Mattathias<br>And His Sons:",
+				    data:{"width": "115","height": "39"},
+				    children:[]			
+			    };
+
+			    var sNode02300011 =  {
+			        id: "s02300011",
+				    name: "Pagan Worship<br>Refused And<br>Resisted:",
+				    data:{"width": "115","height": "57"},
+				    children:[]			
+			    };
+
+			    var sNode02300012 =  {
+			        id: "s02300012",
+				    name: "Farwell Of<br>Mattathias:",
+				    data:{"width": "100","height": "39"},
+				    children:[]			
+			    };
+
+		    var sNode0230001 =  {
+		        id: "s0230001",
+			    name: "Chapter<br>2:",
+			    data:{"width": "95","height": "39"},
+			    children:[sNode02300010,sNode02300011,sNode02300012]			
+		    };
+
+	    var sNode023000 =  {
+	        id: "s023000",
+		    name: "Crisis And<br>Response:",
+		    data:{"width": "95","height": "39"},
+		    children:[sNode0230000,sNode0230001]			
+	    };
+
+	    var sNode023001 =  {
+	        id: "s023001",
+		    name: "Leadership Of<br>Judas Maccabeus:",
+		    data:{"width": "140","height": "39"},
+		    children:[]			
+	    };
+
+    var sNode02300 =  {
+        id: "s02300",
+	    name: "<u>1st Maccabees</u><br>Part One:",
+	    data:{"width": "115","height": "39"},
+	    children:[sNode023000,sNode023001]			
+    };
+
+	    // Children of sNode02301 (<u>1st Maccabees</u><br>Part Two:)
+
+	    var sNode023010 =  {
+	        id: "s023010",
+		    name: "Leadership Of<br>Jonathan:",
+		    data:{"width": "115","height": "39"},
+		    children:[]			
+	    };
+
+	    var sNode023011 =  {
+	        id: "s023011",
+		    name: "Leadership Of<br>Simon:",
+		    data:{"width": "115","height": "39"},
+		    children:[]			
+	    };
+
+    var sNode02301 =  {
+        id: "s02301",
+	    name: "<u>1st Maccabees</u><br>Part Two:",
+	    data:{"width": "115","height": "39"},
+	    children:[sNode023010,sNode023011]			
+    };
+
+/*
+var sNode0230 =  {
+    id: "s0230",
+    name: "1st<br>Maccabees:",
+    data:{"width": "80","height": "57"},
+    children:[sNode02300,sNode02301,sNode02302,sNode02303]			
+}; */
