5c4fe465723c7735b9f670a469cf785927e69083
Bob Englert <mre29073@yahoo.com>
2021-09-09 14:02:42 -0600

    Add files via upload

--- js/Scripture/OT/Wisdom/WisdomBooks.js
+++ js/Scripture/OT/Wisdom/WisdomBooks.js
@@ -1,80 +1,96 @@
-	// Children of sNode02 (The Wisdom Books)
-	
-	        var sNode020 = {
-	            id: "s020",
-		        name: "Job",
-		        data:{"width": "35","height": "20"},
-		        children:[]			
-	        };
+	// Children of sNode03 (The Wisdom Books)
 			
-				// Children of sNode021 (Psalms)		
+				// Children of sNode030 (Job:)		
 		
-                var sNode0210 = {
-	                id: "s0210",
-	                name: "Psalms<br>1,8,13,19,23,27,32",
-	                data:{"width": "120","height": "35"},
-                    children:[sNode02100,sNode02101,sNode02102,sNode02103,sNode02104,sNode02105,sNode02106]			
+                var sNode0300 = {
+	                id: "s0300",
+	                name: "Job<br>1-21:",
+	                data:{"width": "55","height": "39"},
+                    children:[]			
                 };
 
-                var sNode0211 = {
-	                id: "s0211",
-	                name: "Psalms<br>33,51,63,91,95,100,121",
-	                data:{"width": "150","height": "35"},
-	                children:[sNode02110,sNode02111,sNode02112,sNode02113,sNode02114,sNode02115,sNode02116]			
+                var sNode0301 = {
+	                id: "s0301",
+	                name: "Job<br>22-42:",
+	                data:{"width": "60","height": "39"},
+	                children:[]			
                 };
 	
-                var sNode0212 = {
-	                id: "s0212",
+	        var sNode030 = {
+	            id: "s030",
+		        name: "Job:",
+		        data:{"width": "45","height": "23"},
+		        children:[sNode0300,sNode0301]			
+	        };
+			
+				// Children of sNode031 (Psalms)		
+		
+                var sNode0310 = {
+	                id: "s0310",
+	                name: "Psalms<br>1,8,13,19,23,27,32",
+	                data:{"width": "120","height": "39"},
+                    children:[sNode03100,sNode03101,sNode03103,sNode03103,sNode03104,sNode03105,sNode03106]			
+                };
+
+                var sNode0311 = {
+	                id: "s0311",
+	                name: "Psalms<br>33,51,63,91,95,100,121",
+	                data:{"width": "150","height": "39"},
+	                children:[sNode03110,sNode03111,sNode03112,sNode03113,sNode03114,sNode03115,sNode03116]			
+                };
+	
+                var sNode0312 = {
+	                id: "s0312",
 	                name: "Psalms<br>123,126,127,128,<br>130,131,133",
 	                data:{"width": "120","height": "50"},
-	                children:[sNode02120,sNode02121,sNode02122,sNode02123,sNode02124,sNode02125,sNode02126]			
+	                children:[sNode03120,sNode03121,sNode03122,sNode03123,sNode03124,sNode03125,sNode03126]			
                 };
 	
-                var sNode0213 = {
-	                id: "s0213",
+                var sNode0313 = {
+	                id: "s0313",
 	                name: "Psalms<br>134,142,144,146,<br>147,148,150",
                     data:{"width": "120","height": "50"},
-	                children:[sNode02130,sNode02131,sNode02132,sNode02133,sNode02134,sNode02135,sNode02136]			
-                };
+	                children:[sNode03130,sNode03131,sNode03132,sNode03133,sNode03134,sNode03135,sNode03136]			
+                }; 
 	
-            var sNode021 = {
-	            id: "s021",
+            var sNode031 = {
+	            id: "s031",
 	            name: "Psalms",
-	            data:{"width": "50","height": "20"},
-	            children:[sNode0210,sNode0211,sNode0212,sNode0213]			
+	            data:{"width": "50","height": "23"},
+	            children:[sNode0310,sNode0311,sNode0312,sNode0313]			
             };
 				
-	        var sNode022 = {
-	            id: "s022",
-		        name: "Proverbs",
-		        data:{"width": "65","height": "20"},
+	        var sNode032 = {
+	            id: "s032",
+		        name: "Proverbs:",
+		        data:{"width": "80","height": "23"},
 		        children:[]			
 	        };
 				
-	        var sNode023 = {
-	            id: "s023",
-		        name: "Ecclesiastes",
-		        data:{"width": "85","height": "20"},
+	        var sNode033 = {
+	            id: "s033",
+		        name: "Ecclesiastes<br>Song Of Songs:",
+		        data:{"width": "120","height": "39"},
 		        children:[]			
 	        };
 				
-	        var sNode024 = {
-	            id: "s024",
-		        name: "Song Of Songs",
-		        data:{"width": "90","height": "20"},
+	        var sNode034 = {
+	            id: "s034",
+		        name: "Wisdom:",
+		        data:{"width": "70","height": "23"},
 		        children:[]			
 	        };
 				
-	        var sNode025 = {
-	            id: "s025",
-		        name: "Wisdom",
-		        data:{"width": "55","height": "20"},
+	        var sNode035 = {
+	            id: "s035",
+		        name: "Sirach<br>1-26",
+		        data:{"width": "60","height": "39"},
 		        children:[]			
 	        };
 				
-	        var sNode026 = {
-	            id: "s026",
-		        name: "Sirach",
-		        data:{"width": "45","height": "20"},
+	        var sNode036 = {
+	            id: "s036",
+		        name: "Sirach<br>27-51",
+		        data:{"width": "60","height": "39"},
 		        children:[]			
 	        };
