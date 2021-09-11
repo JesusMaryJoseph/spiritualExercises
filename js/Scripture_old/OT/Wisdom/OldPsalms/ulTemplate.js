7ed1a2d6e660a9dc954954aa15f1a7113fe1de74
Bob Englert <mre29073@yahoo.com>
2021-09-11 09:35:18 -0600

    Delete ulTemplate.js

--- js/Scripture_old/OT/Wisdom/OldPsalms/ulTemplate.js
+++
@@ -1,8 +0,0 @@
-//    "<ul>
-//        <li id=\"p_li_\" onclick=\"liClkHndlr(event)\"></li>
-//        <li id=\"p_li_\" onclick=\"liClkHndlr(event)\"></li>
-//	      <li id=\"p_li_\" onclick=\"liClkHndlr(event)\"></li>
-//	      <li id=\"p_li_\" onclick=\"liClkHndlr(event)\"></li>
-//	      <li id=\"p_li_\" onclick=\"liClkHndlr(event)\"></li>
-//	      <li id=\"p_li_\" onclick=\"liClkHndlr(event)\"></li>
-//     </ul>"  
