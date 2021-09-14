47d43e70088283224e68eaf4852e50226fca6ba0
Bob Englert <mre29073@yahoo.com>
2021-09-14 14:10:35 -0600

    Update Classes_Objects_Functions_Global.js

--- js/Classes_Objects_Functions_Global.js
+++ js/Classes_Objects_Functions_Global.js
@@ -267,7 +267,7 @@ var PlayerControl = {
 	// Methods
 	onPlayerStateChange: function(event) {
 		if (event.data == YT.PlayerState.ENDED && player.getVideoLoadedFraction() > 0) {
-		  alert("in onPllayerStateChange, currentVideoId: " + this currentVideoId);
+		  alert("in onPllayerStateChange, currentVideoId: " + this.currentVideoId);
 			if (ButtonState.getState('shuffle') ) {
 				let randomNumber = Math.floor( Math.random() * VideoData.getData(VideoData.selectedSeason).ids.length);
 				while ( this.playedVideos.includes(randomNumber) ) {
@@ -283,7 +283,7 @@ var PlayerControl = {
 				}
 			}
 		 // alert("ready to load, setSongTitle, and play");
-		  alert("currentVideoId is now: " + this currentVideoId);
+		  alert("currentVideoId is now: " + this.currentVideoId);
 			player.loadVideoById(VideoData.getData(VideoData.selectedSeason).ids[this.currentVideoId]);
 			PlayerControl.setSongTitle();
 			PlayerControl.startVideo();
