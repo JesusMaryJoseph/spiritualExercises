bba10d4258fd8c758b033f33a2f6a21af0257d91
Bob Englert <mre29073@yahoo.com>
2024-02-03 12:21:14 -0700

    Update Classes_Objects_Functions_Global.js

--- js/Classes_Objects_Functions_Global.js
+++ js/Classes_Objects_Functions_Global.js
@@ -607,7 +607,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 		width: '600',
-		videoId:'3tqSeu47iIc',
+		videoId:'CDljPsRIryo',
 		playerVars: {
 			controls: 0,
 			autoplay: 1,
