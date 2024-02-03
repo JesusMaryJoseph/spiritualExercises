af6adf813142c37c2f02cd08c94866f22f4451c8
Bob Englert <mre29073@yahoo.com>
2024-02-03 11:49:22 -0700

    Update Data_Media.js

--- js/Data_Media.js
+++ js/Data_Media.js
@@ -867,7 +867,7 @@ var VideoData = {
 		names: [
 			'All Creatures Of Our God And King<br>First Dallas Choir and Orchestra',
 			'We Are Called',
-			'The Church's One Foundation is Jesus Christ Her Lord<br>Origin Ministries',
+			'The Church\'s One Foundation is Jesus Christ Her Lord<br>Origin Ministries',
 			'The Servant Song<br>lyrics by Richard Gillard',
 			'Holy Is His Name<br>Connie Salazar',
 			'O God Beyond All Praising<br>BJU Singers & Orchestry',
