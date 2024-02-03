5e6c8e5fca34c75e98a41bcb24114521a0e15a1d
Bob Englert <mre29073@yahoo.com>
2024-02-03 12:00:59 -0700

    Update Data_Media.js

--- js/Data_Media.js
+++ js/Data_Media.js
@@ -831,9 +831,9 @@ var VideoData = {
 
 	ordinary: {
 		ids: [
-			/*'eETRRbLLuMM',
+			/*'eETRRbLLuMM',*/
 			'CDljPsRIryo',
-			{'videoId':'BA-JXeHDRDw', 'endSeconds': 268},*/
+			{'videoId':'BA-JXeHDRDw', 'endSeconds': 268},
 			'kdmgpMfnjdU',
 			'3tqSeu47iIc',
 			'TymGK7HzbwI',
@@ -865,9 +865,9 @@ var VideoData = {
 		],
 
 		names: [
-			/*'All Creatures Of Our God And King<br>First Dallas Choir and Orchestra',
+			/*'All Creatures Of Our God And King<br>First Dallas Choir and Orchestra',*/
 			'We Are Called',
-			'The Churchs One Foundation is Jesus Christ Her Lord<br>Origin Ministries', */
+			'The Churchs One Foundation is Jesus Christ Her Lord<br>Origin Ministries', 
 			'The Servant Song<br>lyrics by Richard Gillard', 
 			'Holy Is His Name<br>Connie Salazar',
 			'O God Beyond All Praising<br>BJU Singers & Orchestry',
